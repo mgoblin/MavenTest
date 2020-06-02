683965f37410466ccce35e24250b977d46b58670
mgoblin <mike.golovanov@gmail.com>
2020-06-02 22:44:36 +0300

    test1

---
+++ src/test/java/ru/arl/Test1.java
@@ -0,0 +1,12 @@
+package ru.arl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Test1 {
+    @Test
+    public void test1() {
+        assertEquals(2, 1);
+    }
+}
